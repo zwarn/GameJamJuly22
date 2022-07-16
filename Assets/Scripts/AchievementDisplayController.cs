25d4badd1dee27a36874737ad4eaa88aae68bf74
Miautomat <m.accounts@mailfence.com>
2022-07-16 18:11:31 +0200

    add first achievements

--- Assets/Scripts/AchievementDisplayController.cs
+++ Assets/Scripts/AchievementDisplayController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AchievementDisplayController : MonoBehaviour
 {
+    public List<Achievement> achievements;
+    
     // Start is called before the first frame update
     void Start()
     {
