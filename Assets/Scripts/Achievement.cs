9c439c045d6e790cb4775c822be81a4ff6b73561
Miautomat <m.accounts@mailfence.com>
2022-07-16 17:16:26 +0200

    init: AchievementDisplay Setup
    x

---
+++ Assets/Scripts/Achievement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Achievement", menuName = "Achievement", order = 0)]
+public class Achievement : ScriptableObject
+{
+    public Sprite icon;
+    public bool hasBeenAchieved = false;
+
+    public void SetAchievement()
+    {
+        this.hasBeenAchieved = true;
+    }
+}
