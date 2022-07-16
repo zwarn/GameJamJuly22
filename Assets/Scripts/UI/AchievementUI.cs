6fda14db610d7df38d0611d5084b5063a4b1c711
Miautomat <m.accounts@mailfence.com>
2022-07-16 18:41:42 +0200

    add AchievementUI non-functional

---
+++ Assets/Scripts/UI/AchievementUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UI
+{
+    public class AchievementUI : MonoBehaviour
+    {
+        public List<Achievement> achievements;
+    
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach (Achievement achievement in achievements)
+            {
+            
+            }
+        }
+    }
+}
+
