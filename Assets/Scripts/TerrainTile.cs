cda584c969a073205e6e743adf6485c8da554313
Michael Budahn <michael.budahn@gmail.com>
2022-07-16 14:19:09 +0200

    create tile scriptable objects

---
+++ Assets/Scripts/TerrainTile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(fileName = "Tile", menuName = "TerrainTile", order = 0)]
+public class TerrainTile : ScriptableObject
+{
+    public TerrainType type;
+    public Tile tile;
+}
+
+[Serializable]
+public enum TerrainType
+{
+    Water,
+    Forest,
+    Meadows,
+    Sand,
+    Dirt,
+    Rock,
+    Swamp,
+    Tropics,
+    Snow,
+}
