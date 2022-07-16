6a0d9ff3c6d5a900415e72680c4b2b12ad6d37ef
Michael Budahn <michael.budahn@gmail.com>
2022-07-16 18:46:48 +0200

    show current inventory count

---
+++ Assets/Scripts/UI/InventoryUI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace UI
+{
+    public class InventoryUI : MonoBehaviour
+    {
+        public TMP_Text stoneText;
+        public TMP_Text woodText;
+
+        private InventoryController _inventoryController;
+
+        private void Start()
+        {
+            _inventoryController = InventoryController.Instance();
+            Events.Instance().MoveMade += UpdateCount;
+            UpdateCount();
+        }
+
+
+        private void UpdateCount()
+        {
+            stoneText.text = _inventoryController.GetResourceCount(ResourceType.Stone).ToString();
+            woodText.text = _inventoryController.GetResourceCount(ResourceType.Wood).ToString();
+        }
+    }
+}
